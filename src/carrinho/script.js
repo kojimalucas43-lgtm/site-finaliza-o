2a045f53f9009f58806dc22daa5ebbe9619e6360
noahdondacasagrande-glitch <noahdondacasagrande@gmail.com>
2026-08-23 20:38:45 -0300

    Update order generation with delivery address
    Added confirmation message for delivery address in order generation.

--- src/carrinho/script.js
+++ src/carrinho/script.js
@@ -48,6 +48,8 @@ function gerar(){
                 <h1>PEDIDO CONFIRMADO</h1>
                 <h3>Agradecemos a sua compra e sua preferência.</h3>
                 <br>
+                <h3>O se podido vai ser entregue em sesi ltda, rua paula gomes</h3>
+                <br>
                 ${listaHtml}
                 <br>
                 <br>
